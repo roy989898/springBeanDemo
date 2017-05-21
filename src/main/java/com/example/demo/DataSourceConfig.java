a5c771762c2e17adb20140a16c09f4a62b7781cb
poming\pomingpo <roy989898@gmail.com>
2017-05-21 19:49:57 +0800

    print the Datasource Bean

--- src/main/java/com/example/demo/DataSourceConfig.java
+++ src/main/java/com/example/demo/DataSourceConfig.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created by pomingpo on 2017/5/21.
@@ -9,8 +10,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataSourceConfig {
 
-    @Bean
-    DataSource dataSource() {
-        return new DataSource("myurl", 9999);
+    @Bean(name = "datasource")
+    @Profile("development")
+    DataSource development() {
+        return new DataSource("my-dev-url", 9999);
+    }
+
+
+    @Bean(name = "datasource")
+    @Profile("production")
+    DataSource production() {
+        return new DataSource("my-production-url", 9999);
     }
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+
+        System.out.println(ctx.getBean("datasource"));
+
+    }
 }
